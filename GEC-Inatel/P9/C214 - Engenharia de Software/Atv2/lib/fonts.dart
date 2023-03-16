4f3b1b41c06f996af3a460d3bca200977f1062f3
Pedro Balestra <pedro.balestra@outlook.com>
2023-03-15 23:46:28 -0300

    Implementação de TextFormFields com as fontes da Google

--- GEC-Inatel/P9/C214 - Engenharia de Software/Atv2/lib/fonts.dart
+++ GEC-Inatel/P9/C214 - Engenharia de Software/Atv2/lib/fonts.dart
@@ -12,53 +12,109 @@ class _FontsState extends State<Fonts> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text(
           "Google Fonts",
           style: TextStyle(fontSize: 25),
         ),
         centerTitle: true,
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.black,
       ),
       body: ListView(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
           Container(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(50),
             alignment: Alignment.center,
             child: const Text(
               "Aplicação para mostrar o uso da dependecia do google fonts",
               style: TextStyle(fontSize: 20),
             ),
           ),
-          SizedBox(
-            height: 50,
+          const SizedBox(
+            height: 20,
           ),
           Container(
-              padding: EdgeInsets.all(20),
-              alignment: Alignment.center,
-              child: Text("Exemplo Fonte 1",
-                  style: GoogleFonts.abel(fontSize: 30, color: Colors.red),
-
-          )
+            padding: const EdgeInsets.only(left: 30),
+            child: Text(
+              "Exemplo Fonte Abel",
+              style: GoogleFonts.abel(fontSize: 25, color: Colors.black87),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
           ),
           Container(
-              padding: EdgeInsets.all(20),
-              alignment: Alignment.center,
-              child: Text("Exemplo Fonte 2",
-                style: GoogleFonts.abyssinicaSil(fontSize: 30,color: Colors.blue)
-
-              )
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide:
+                        const BorderSide(color: Colors.black, width: 2.5),
+                    borderRadius: BorderRadius.circular(10.0)),
+                labelText: "Escreva aqui",
+                labelStyle: const TextStyle(color: Colors.black),
+              ),
+              style: GoogleFonts.abel(fontSize: 20, color: Colors.black),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
           ),
           Container(
-              padding: EdgeInsets.all(20),
-              alignment: Alignment.center,
-              child: Text("Exemplo Fonte 3",
-                  style: GoogleFonts.b612Mono(fontSize: 30,color: Colors.orange)
-
-              )
+              padding: const EdgeInsets.only(left: 30),
+              child: Text(
+                "Exemplo Fonte AbyssinicaSil",
+                style: GoogleFonts.abyssinicaSil(
+                    fontSize: 25, color: Colors.black87),
+              )),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide:
+                        const BorderSide(color: Colors.black, width: 2.5),
+                    borderRadius: BorderRadius.circular(10.0)),
+                labelText: "Escreva aqui",
+                labelStyle: const TextStyle(color: Colors.black),
+              ),
+              style:
+                  GoogleFonts.abyssinicaSil(fontSize: 20, color: Colors.black),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 30),
+            child: Text(
+              "Exemplo Fonte B612Mono",
+              style: GoogleFonts.b612Mono(fontSize: 25, color: Colors.black87),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide:
+                        const BorderSide(color: Colors.black, width: 2.5),
+                    borderRadius: BorderRadius.circular(10.0)),
+                labelText: "Escreva aqui",
+                labelStyle: const TextStyle(color: Colors.black),
+              ),
+              style: GoogleFonts.b612Mono(fontSize: 20, color: Colors.black),
+            ),
           ),
         ],
       ),
